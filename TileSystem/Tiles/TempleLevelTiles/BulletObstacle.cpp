84bf89c0ad15fa9e602fcf0d28c97f12592e3529
LucasEBSkora <skora.lucas@gmail.com>
2019-11-14 23:19:55 -0300

    logica para balas terminada, em teoria, so falta colocar elas de verdade

--- TileSystem/Tiles/TempleLevelTiles/BulletObstacle.cpp
+++ TileSystem/Tiles/TempleLevelTiles/BulletObstacle.cpp
@@ -20,18 +20,46 @@ namespace DIM {
       
       unsigned int i = positionMine.y - 1, j = positionMine.x;
 
+      bool searchLeft, searchRight;
+
       try {
-        if (map[i][j-1] == 0) {
-          //procurar para a direita
-        }
+      
+        searchLeft = map[i][j-1] != 0;
+      
+      } catch (std::string s) {
+      
+        searchLeft = false;
+      
+      }
+      
+      try {
+
+       searchRight = map[i][j+1] !=0;
 
       } catch (std::string s) {
+        
+        searchRight = false;
+      
+      }
+      
+      bool foundLeft = false, foundRight = false;
 
+      for (unsigned int k = 2; (searchLeft || searchRight) && (!foundLeft && !foundRight); ++k) {
+        if (j-k < 0) searchLeft = false;
+        if (j+k > map.getSize.x - 1) searchRight = false;
+
+        if (searchLeft) foundLeft = map[i][j-k] == 0; 
+        if (searchRight) foundRight = map[i][j+k] == 0;
       }
 
-      
+      if (searchLeft) {
+        //create arrow at (i, j-k) going right
+
+      } else if (searchRight) {
+        //create arrow at (i, j+k) going left
+      }
 
     }
-
   }
+
 }
